ef72584976bff57b597304b99fd5e9a330ef901a
unknown <ikien.dang@gmail.com>
2022-01-31 20:27:30 -0500

    spotify storage mapping

--- index.js
+++ index.js
@@ -71,17 +71,15 @@ app.get("/callback", (req, res) => {
   })
     .then((response) => {
       if (response.status === 200) {
-        const { access_token, refresh_token } = response.data;
+        const { access_token, refresh_token, expires_in } = response.data;
 
         const queryParams = querystring.stringify({
           access_token,
           refresh_token,
+          expires_in,
         });
 
-        //redirect to react app
         res.redirect(`http://localhost:3000/?${queryParams}`);
-
-        //pass along tokens in query params
       } else {
         res.redirect(`/?${querystring.stringify({ error: "invalid_token" })}`);
       }
