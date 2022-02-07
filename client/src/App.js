fb1a10f29dcc339ae98397f03ae45d72f88ccddc
unknown <ikien.dang@gmail.com>
2022-02-06 22:07:17 -0500

    login styling refactor complete

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,74 @@
 import { useState, useEffect } from "react";
-import { accessToken, logout } from "./spotify";
-import "./App.css";
+import { accessToken, logout, getCurrentUserProfile } from "./spotify";
+import { BrowserRouter as Router, Switch, useLocation } from "react-router-dom";
+import { catchErrors } from "./utils";
+import styled from "styled-components/macro";
+import { GlobalStyle } from "./styles";
+import { Login } from "./pages";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   const [token, setToken] = useState(null);
+  const [profile, setProfile] = useState(null);
 
   useEffect(() => {
     setToken(accessToken);
+
+    const fetchData = async () => {
+      const { data } = await getCurrentUserProfile();
+      setProfile(data);
+    };
+
+    catchErrors(fetchData());
   }, []);
 
   return (
     <div className="App">
+      <GlobalStyle />
       <header className="App-header">
         {!token ? (
-          <a
-            className="App-link"
-            href="http://localhost:8888/login"
-            rel="noopener noreferrer"
-          >
-            Log in Spotify
-          </a>
+          <Login />
         ) : (
-          <>
-            <h1>Logged in</h1>
-            <button onClick={logout}>Log Ou t</button>
-          </>
+          <Router>
+            <ScrollToTop />
+            <Switch>
+              <Router path="/top-artists">
+                <h1>Top Artists</h1>
+              </Router>
+              <Router path="/top-tracks">
+                <h1>Top Tracks</h1>
+              </Router>
+              <Router path="/playlist/:id">
+                <h1>Playlist</h1>
+              </Router>
+              <Router path="/playlist">
+                <h1>Playlist Route</h1>
+              </Router>
+              <Router path="/">
+                <>
+                  <button onClick={logout}>Log Out</button>
+                  {profile && (
+                    <div>
+                      <h1>{profile.display_name}</h1>
+                      <p>{profile.followers.total} Followers</p>
+                      {profile.followers.total && profile.images[0].url && (
+                        <img src={profile.images[0].url} alt="Avatar" />
+                      )}
+                    </div>
+                  )}
+                </>
+              </Router>
+            </Switch>
+          </Router>
         )}
       </header>
     </div>
